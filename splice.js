97ea5ad977745d2163e2c8dbcf7723817d8b3004
Arsalan Imran <aaghc114@gmail.com>
2026-06-27 02:11:26 +0500

    slice arrays method

---
+++ splice.js
@@ -0,0 +1,8 @@
+const arr = [1,2,3,4,5,6,7,8,9,10]
+// const removed = arr.splice(0,6)
+
+// console.log(removed)
+
+const addArray= arr.splice(1,6,"a", "b", "c")
+console.log(addArray)
+console.log(arr)
